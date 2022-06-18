48e53c4b6bffc57dc9e51e76d92b78a4d9d2258a
Luca Zambarda <luca@blokur.com>
2022-06-18 09:23:43 +0100

    feat: add binding for MIDI driver

--- driver.go
+++ driver.go
@@ -8,6 +8,7 @@ package fluidsynth
 typedef void (*closure)();
 */
 import "C"
+import "unsafe"
 
 type AudioDriver struct {
 	ptr *C.fluid_audio_driver_t
@@ -35,4 +36,16 @@ func (r *FileRenderer) Delete() {
 
 func (r *FileRenderer) ProcessBlock() bool {
 	return C.fluid_file_renderer_process_block(r.ptr) == C.FLUID_OK
+}
+
+type MIDIDriver struct {
+	ptr *C.fluid_midi_driver_t
+}
+
+func NewMIDIDriver(settings Settings, synth Synth) MIDIDriver {
+	return MIDIDriver{C.new_fluid_midi_driver(settings.ptr, C.closure(C.fluid_synth_handle_midi_event), unsafe.Pointer(synth.ptr))}
+}
+
+func (d *MIDIDriver) Delete() {
+	C.delete_fluid_midi_driver(d.ptr)
 }
